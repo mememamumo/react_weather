4d1f122e69dd5dd95cc06aa92b68e5dcf5ab21cc
Yuna Lee <carnivalss@naver.com>
2020-07-17 16:34:45 +0900

    Loading

---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Getting the react weather :-D</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "flex-end",
+		paddingHorizontal: 30,
+		paddingVertical: 100,
+		backgroundColor: "#c8c0f6"
+	},
+	text: {
+		color: "#000000",
+		fontSize: 30
+	}
+})
